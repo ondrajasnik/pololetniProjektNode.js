e7f4274a7bbf5bc9962d0cd80804546007614c2e
ondrajasnik <122598530+ondrajasnik@users.noreply.github.com>
2024-06-18 12:40:34 +0200

    Update Particle.js

--- public/js/classes/Particle.js
+++ public/js/classes/Particle.js
@@ -1,30 +1,36 @@
+// Konstantní hodnota pro tření, která zpomaluje rychlost částice
 const friction = 0.99
+
+// Definice třídy Particle pro reprezentaci částice
 class Particle {
+  // Konstruktor inicializuje částici s danými parametry
   constructor(x, y, radius, color, velocity) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.velocity = velocity
-    this.alpha = 1
+    this.x = x  // X-ová souřadnice částice
+    this.y = y  // Y-ová souřadnice částice
+    this.radius = radius  // Poloměr částice
+    this.color = color  // Barva částice
+    this.velocity = velocity  // Rychlost částice (objekt s vlastnostmi x a y)
+    this.alpha = 1  // Průhlednost částice, počáteční hodnota je 1 (plně viditelná)
   }
 
+  // Metoda pro vykreslení částice
   draw() {
-    c.save()
-    c.globalAlpha = this.alpha
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.restore()
+    c.save()  // Uloží aktuální stav kontextu
+    c.globalAlpha = this.alpha  // Nastaví průhlednost kontextu
+    c.beginPath()  // Zahájí novou cestu pro kreslení
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)  // Vykreslí kruh s danými parametry
+    c.fillStyle = this.color  // Nastaví barvu výplně
+    c.fill()  // Vyplní kruh
+    c.restore()  // Obnoví předchozí stav kontextu
   }
 
+  // Metoda pro aktualizaci stavu částice
   update() {
-    this.draw()
-    this.velocity.x *= friction
-    this.velocity.y *= friction
-    this.x = this.x + this.velocity.x
-    this.y = this.y + this.velocity.y
-    this.alpha -= 0.01
+    this.draw()  // Vykreslí částici
+    this.velocity.x *= friction  // Aplikuje tření na X-ovou složku rychlosti
+    this.velocity.y *= friction  // Aplikuje tření na Y-ovou složku rychlosti
+    this.x = this.x + this.velocity.x  // Aktualizuje X-ovou pozici částice
+    this.y = this.y + this.velocity.y  // Aktualizuje Y-ovou pozici částice
+    this.alpha -= 0.01  // Sníží průhlednost částice pro vytvoření efektu mizení
   }
 }
